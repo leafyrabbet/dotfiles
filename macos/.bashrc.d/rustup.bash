faa4272dae8818f046b360db340e54bfef32e1e2
Tommy Vegetables <leafyrabbet@gmail.com>
2021-09-05 22:05:25 -0400

    NEW Adding rustup Config
    - Rust management

---
+++ macos/.bashrc.d/rustup.bash
@@ -0,0 +1,9 @@
+# Multiple Concurrent Rust (Cargo) Installs
+#
+# Reference:
+# - https://rustup.rs/
+#
+# Install:
+# `brew install rustup`
+# `rustup-init` -- Follow Instructions to finish install
+export PATH="${HOME}/.cargo/bin:${PATH}"
